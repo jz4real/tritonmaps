dd59a83c76e9dc620f681325991c10fb1cf06826
jz4real <jeffreyzhang28@gmail.com>
2023-07-20 19:50:14 -0700

    implement addAll

--- src/autocomplete/SequentialSearchAutocomplete.java
+++ src/autocomplete/SequentialSearchAutocomplete.java
@@ -24,8 +24,7 @@ public class SequentialSearchAutocomplete implements Autocomplete {
 
     @Override
     public void addAll(Collection<? extends CharSequence> terms) {
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        elements.addAll(terms);
     }
 
     @Override
